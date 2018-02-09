246bc4e5c0bc515e75dff8ccd843d6c40076bc58
Max Rupplin - Sr. Scientologist <mearvk@outlook.com>
2018-02-09 14:39:01 -0500

    Latest version

---
+++ src/webcrawler/exceptions/TayException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webcrawler.exceptions;
+
+/**
+ *
+ * @author oem
+ */
+public class TayException extends Exception
+{
+    public TayException()
+    {
+        super("Two meanings; one nay.");
+    }       
+}
